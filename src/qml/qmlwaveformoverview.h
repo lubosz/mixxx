0f1be4c62c17d42fa6810d1501a0a587587147a5
Lubosz Sarnecki <lubosz@gmail.com>
2025-04-18 10:17:17 +0200

    qmlwaveform: Drop namespace prefixes for member notify signal parameters.
    Remove absolute scope annotations in class parameters for the
    rendererChanged signal.
    
    Fixes build with Qt 6.9.0.
    
    See https://github.com/mixxxdj/mixxx/issues/14122

--- src/qml/qmlwaveformoverview.h
+++ src/qml/qmlwaveformoverview.h
@@ -58,7 +58,7 @@ class QmlWaveformOverview : public QQuickPaintedItem {
   signals:
     void playerChanged();
     void channelsChanged(mixxx::qml::QmlWaveformOverview::Channels channels);
-    void rendererChanged(mixxx::qml::QmlWaveformOverview::Renderer renderer);
+    void rendererChanged(Renderer renderer);
     void colorHighChanged(const QColor& color);
     void colorMidChanged(const QColor& color);
     void colorLowChanged(const QColor& color);
